6054227b3783a8f7db4fe7929ce7e8be54e5e9e6
georgyhim1997@yandex.ru <georgyhim1997>
2020-11-28 15:30:08 +0300

    Write Main

---
+++ ToDoList/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.TestServlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server = createServer(8080);
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+
+    private static Server createServer(int port) {
+        TestServlet testServlet = new TestServlet();
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        handler.addServlet(new ServletHolder(testServlet), "/test");
+        Server server = new Server(port);
+        server.setHandler(handler);
+        return server;
+    }
+}
