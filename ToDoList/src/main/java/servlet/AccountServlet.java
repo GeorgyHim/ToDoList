cbb91916dd67d2f00689413ca6a2c75d849eb30b
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-12 13:30:42 +0300

    Extract base work with response to BaseServlet class

--- ToDoList/src/main/java/servlet/AccountServlet.java
+++ ToDoList/src/main/java/servlet/AccountServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public abstract class AccountServlet extends HttpServlet {
+public abstract class AccountServlet extends BaseServlet {
 
     /** Ссылка на {@link service.AccountService} */
     protected final AccountService accountService;
@@ -15,19 +15,4 @@ public abstract class AccountServlet extends HttpServlet {
         this.accountService = accountService;
     }
 
-
-    /**
-     * Метод установления нужного типа контента для response
-     */
-    protected void setContentType(HttpServletResponse response) {
-        response.setContentType("text/html;charset=utf-8");
-    }
-
-    /**
-     * Запись объекта user в response в формате json
-     */
-    protected void returnData(HttpServletResponse response, String data) throws IOException {
-        response.getWriter().println(data);
-        response.setStatus(HttpServletResponse.SC_OK);
-    }
 }
---
+++ ToDoList/src/main/java/servlet/BaseServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class BaseServlet extends HttpServlet {
+    /**
+     * Метод установления нужного типа контента для response
+     */
+    protected void setContentType(HttpServletResponse response) {
+        response.setContentType("text/html;charset=utf-8");
+    }
+
+    /**
+     * Запись объекта user в response в формате json
+     */
+    protected void returnData(HttpServletResponse response, String data) throws IOException {
+        response.getWriter().println(data);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
