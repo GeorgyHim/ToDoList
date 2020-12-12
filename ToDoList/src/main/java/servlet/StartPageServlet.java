2d639d331786d014112cf8b08a7688409c5443ad
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-12 18:52:08 +0300

    Write StartPageServlet

---
+++ ToDoList/src/main/java/servlet/StartPageServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import model.User;
+import service.AccountService;
+import servlet.base.AccountServlet;
+import util.exception.UserNotAuthorized;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class StartPageServlet extends AccountServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String sessionId = req.getSession().getId();
+        try {
+            User user = AccountService.getInstance().getAuthorizedUser(sessionId);
+            resp.sendRedirect("/tasks/all");
+        } catch (UserNotAuthorized ignored) {}
+        super.service(req, resp);
+    }
+}
