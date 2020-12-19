6760f71e984524616b42543b4f13843178f375d2
georgyhim1997@yandex.ru <georgyhim1997>
2020-12-19 17:33:20 +0300

    Add tasks data in TasksServlet and tasks.html

--- ToDoList/src/main/java/servlet/tasks/TasksServlet.java
+++ ToDoList/src/main/java/servlet/tasks/TasksServlet.java
@@ -1,17 +1,28 @@
 package servlet.tasks;
 
+import model.Task;
+import model.ToDoList;
 import servlet.abstracts.UserServlet;
 import util.templater.PageGenerator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class TasksServlet extends UserServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Task> tasks = new ArrayList<>();
+        for (ToDoList toDoList : this.user.getToDoLists()) {
+            tasks.addAll(toDoList.getTasks());
+        }
+        String jsonData = mapper.writeValueAsString(tasks);
         setHtmlContent(resp);
-        resp.getWriter().println(PageGenerator.getInstance().renderPage("tasks.html", Collections.emptyMap()));
+        resp.getWriter().println(
+                PageGenerator.getInstance().renderPage("tasks.html", Collections.singletonMap("data", jsonData))
+        );
     }
 }
